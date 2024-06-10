65485c82cd46638f86eb1cd856f2ed4e6101fade
Avi Craimer <mailavi@gmail.com>
2024-06-10 12:46:29 -0400

    Work in progress, still lots of errors with the type universes

--- LeanMathLogicExploration/RelationalAlgebra.lean
+++ LeanMathLogicExploration/RelationalAlgebra.lean
@@ -6,48 +6,49 @@ universe u v
 abbrev Relation.Pairs α β  := (a:α) -> (b:β) -> Prop
 
 -- The Relation inductive type gives the syntactic composition structure of relations. Relation.eval defines the semantic domain for this syntax.
-inductive Relation : (Dom Cod : Type u) -> Type (u + 1)
+inductive Relation :  (Dom : Type _) -> (Cod : Type _) -> Type _
 -- atomic forms a relation directly from a set of pairs
 | atomic (f:Relation.Pairs α β) : Relation α β
 
 -- pair forms a relation as a pair of two values. This is useful for forming higher-order relations from existing relations.
-| pair {α β: Type u} (a: α)(b: β): Relation α β
+| pair   (a: α)(b: β): Relation α β
 
 -- comp stands for composition, and it is the sequential composition operation, which is defined analogously to function composition.
-| comp (R:Relation α β) (S:Relation β γ) : Relation α γ
+| comp   (R:Relation α β) (S:Relation β γ) : Relation α γ
 
 -- converse is one of the involutions of relations, it reverses the direction of the pairs.
-| converse (R:Relation α β) : Relation β α
+| converse  (R:Relation α β) : Relation β α
 
 -- complement is the other involution, it consists of the set theoretic complement of pairs relative to a given relation.
-| complement (R:Relation α β) : Relation α β
+| complement  (R:Relation α β) : Relation α β
 
 -- full is the relation which is the full subset of the Cartersian product of domain and codomain. It's complement is an empty relation.
-| full (α β: Type u): Relation α β
+| full (α β ): Relation α β
 
 -- product is a monoidal product in the category Rel. It corresponds to one of the conjunction operations in linear logic, usually represented as ⊗.
-| product (R: Relation α β )(S: Relation γ δ) : Relation (α × γ ) (β × δ)
+| product  (R: Relation α β )(S: Relation γ δ) : Relation (α × γ ) (β × δ)
 
 -- This is the coproduct in the category Rel. It corresponds to one of the disjunction operations in linear logic, usually represented as ⊕. It is interpreted as a disjoint union of domain, codomain, and relational pairs.
-| coproduct (R: Relation α β )(S: Relation γ δ) : Relation (Sum α γ ) (Sum β δ)
+| coproduct  (R: Relation α β )(S: Relation γ δ) : Relation (Sum α γ ) (Sum β δ)
 
 -- Copy is the diagonal relation, connecting each value in the domain to a pair of identical copies in the codomain. The converse of this is a "merge" relation that sents pairs of identicals to a single copy.  The converse complement (linear negation) of copy is a "different" relation that sends pairs of different elements to all elements.
-| copy (α :Type u): Relation α (α × α)
+| copy (α): Relation α (α × α)
 
 -- cocopy is the categorical dual of copy.  It relates the left and right values of a reflexitve sum type to their common value. This allows us to collapse or merge the disjoint sets of the Sum into a single set which is used to define union. The converse is a "split" relation that splits a single value into two parallel copies in disjoint sets.
-| cocopy (α: Type u): Relation (Sum α α) α
+| cocopy (α): Relation (Sum α α) α
 
 -- First is a projection relation from a pair in the domain to the first member of the pair. The converse inserts a value into all pairs where it occurs in first position.
-| first (α β: Type u): Relation (α × β) α
+| first (α) ( β) : Relation (α × β) α
+-- | first (α : Type u) (β : Type  v ) : ( @[max u v] Relation (α × β) α)
 
 -- Second is a projection relation from a pair in the domain to the second member of the pair. The converse inserts a value into all pairs where it occurs in second position.
-| second (α β: Type u): Relation (α × β) β
+| second (α β ): Relation (α × β) β
 
 -- Left is an injection relation from a value to itself in the left side of a sum type. The converse is a kind of first projection that works with Sum types.
-| left (α β :Type u): Relation α (Sum α β)
+| left (α β ): Relation α (Sum α β)
 
 -- Right is an injection relation from a value to itself in the right side of a sum type. The converse is a kind of second projection that works with Sum types.
-| right (α β :Type u): Relation α (Sum β α)
+| right (α β ): Relation α (Sum β α)
 
 open Relation
 
@@ -108,7 +109,18 @@ match R with
   | Sum.inr a' => a = a'
   | _ => False
 
+
+-- This expresses the eval function as a relation; evalRel relates Relations to Relation Pairs
+-- def Relation.evalRel (α β: Type u): Relation (Relation α β) (Relation.Pairs α β) := atomic (fun R f =>  (eval R) = f)
+
+-- def Relation.evalRel {α β: Type u}: Relation (Relation α β) (Relation.Pairs α β) := atomic (fun R f =>  (eval R) = f)
+
+def Relation.evalRel {α : Type (max u (v+1))} {β : Type v}  : Relation (Relation α β) (Relation.Pairs α β) :=
+  atomic (fun R f => (eval R) = f)
+
 -- **DEFINED RELATION OPERATIONS** --
+
+
 
 -- Merge is the converse of copy
 def Relation.merge (α) := converse (copy α)
